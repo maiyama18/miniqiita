cc25b9bdb781c063a9dd0caeb692f35d721b0e39
muiscript <myam0118@gmail.com>
2019-04-02 00:13:07 +0900

    add utils newRequest/doRequest

--- miniqiita.go
+++ miniqiita.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -46,46 +47,22 @@ type Item struct {
 }
 
 func (c *Client) GetUserItems(ctx context.Context, userID string, page, perPage int) ([]*Item, error) {
-	reqURL := *c.BaseURL
-
-	// set path
-	reqURL.Path = path.Join(reqURL.Path, "users", userID, "items")
-
-	// set query
-	q := reqURL.Query()
-	q.Add("page", strconv.Itoa(page))
-	q.Add("per_page", strconv.Itoa(perPage))
-	reqURL.RawQuery = q.Encode()
-
-	// instantiate request
-	req, err := http.NewRequest(http.MethodGet, reqURL.String(), nil)
+	relativePath := path.Join("users", userID, "items")
+	queries := map[string]string{
+		"page":     strconv.Itoa(page),
+		"per_page": strconv.Itoa(perPage),
+	}
+	req, err := c.newRequest(ctx, http.MethodGet, relativePath, queries, nil, nil)
 	if err != nil {
 		return nil, err
 	}
-
-	// set header
-	req.Header.Set("User-Agent", "qiita-go-client")
-
-	// set context
-	req = req.WithContext(ctx)
 
 	// send request
-	resp, err := c.HTTPClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
+	var items []*Item
+	code, err := c.doRequest(req, &items)
 
-	switch resp.StatusCode {
+	switch code {
 	case http.StatusOK:
-		bodyBytes, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return nil, err
-		}
-		var items []*Item
-		if err := json.Unmarshal(bodyBytes, &items); err != nil {
-			return nil, err
-		}
 		return items, nil
 	case http.StatusBadRequest:
 		return nil, errors.New("bad request. some parameters may be invalid")
@@ -94,4 +71,64 @@ func (c *Client) GetUserItems(ctx context.Context, userID string, page, perPage 
 	default:
 		return nil, errors.New("unexpected error")
 	}
+}
+
+func (c *Client) newRequest(ctx context.Context, method, relativePath string, queries, headers map[string]string, reqBody io.Reader) (*http.Request, error) {
+	reqURL := *c.BaseURL
+
+	// set path
+	reqURL.Path = path.Join(reqURL.Path, relativePath)
+
+	// set query
+	if queries != nil {
+		q := reqURL.Query()
+		for k, v := range queries {
+			q.Add(k, v)
+		}
+		reqURL.RawQuery = q.Encode()
+	}
+
+	// instantiate request
+	req, err := http.NewRequest(method, reqURL.String(), reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	// set header
+	req.Header.Set("User-Agent", "qiita-go-client")
+	req.Header.Set("Authorization", "Bearer "+c.Token)
+	req.Header.Set("Content-Type", "application/json")
+	if headers != nil {
+		for k, v := range headers {
+			req.Header.Set(k, v)
+		}
+	}
+
+	// set context
+	req = req.WithContext(ctx)
+
+	return req, nil
+}
+
+func (c *Client) doRequest(req *http.Request, respBody interface{}) (int, error) {
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode < 200 || 300 <= resp.StatusCode {
+		return resp.StatusCode, nil
+	}
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return 0, err
+	}
+
+	if err := json.Unmarshal(bodyBytes, respBody); err != nil {
+		return 0, err
+	}
+
+	return resp.StatusCode, nil
 }
